86a47ab27b8cfb1917a55ac84227e3c490472ab3
Jérôme Carretero <cJ@zougloub.eu>
2017-02-05 02:35:49 -0500

    conversion_computations: add YCbCr conversions symbolic code

--- conversions_computations.py
+++ conversions_computations.py
@@ -8,7 +8,12 @@ def do_yuv():
 	"""
 	Perform YUV <-> RGB computations.
 
-	The luma and chroma components in YCbCr are calculated from gamma corrected RGB.
+	The luma and chroma components in YUV are calculated from gamma corrected RGB.
+
+	References:
+
+	- https://en.wikipedia.org/wiki/YUV#SDTV_with_BT.601
+
 	"""
 
 	"""
@@ -97,6 +102,105 @@ def do_yuv():
 	"""
 
 
+def do_ycbcr():
+	"""
+	Perform YCbCr <-> RGB computations.
+
+	The luma and chroma components in YCbCr are calculated from gamma corrected RGB.
+
+	References:
+
+	- https://en.wikipedia.org/wiki/YCbCr
+
+	"""
+
+	"""
+	BT.601 defines the following constants:
+
+	- W_R = 0.299
+	- W_B = 0.114
+	- W_G = 1 - W_R - W_B
+	- U_{max} = 0.5
+	- V_{max} = 0.5
+	- Y = W_R * R + W_G * G + W_B * B
+	- U = U_{max} * (B - Y) / (1 - W_B)
+	- V = V_{max} * (R - Y) / (1 - W_R)
+
+	"""
+
+	R, G, B, W_R, W_G, W_B, U_max, V_max, Y, U, V \
+	 = sympy.symbols("R, G, B, W_R, W_G, W_B, U_max, V_max, Y, U, V")
+
+	e1 = sympy.Eq(W_R, 0.299)
+	e2 = sympy.Eq(W_B, 0.114)
+	e3 = sympy.Eq(W_G, 1 - W_R - W_B)
+	e4 = sympy.Eq(U_max, 0.5)
+	e5 = sympy.Eq(V_max, 0.5)
+	e6 = sympy.Eq(U, U_max * (B - Y) / (1 - W_B))
+	e7 = sympy.Eq(V, V_max * (R - Y) / (1 - W_R))
+	e8 = sympy.Eq(Y, R * W_R + G * W_G + B * W_B)
+
+	x = sympy.solve([e1, e2, e3], [W_R, W_B, W_G])
+	print(x)
+	for name in ("W_R", "W_G", "W_B"):
+		for k, v in x.items():
+			if k.name == name:
+				print("%s = %s" % (k, v))
+
+	"""
+	RGB2YUV
+	"""
+
+	x = sympy.solve(
+	 [e1, e2, e3, e4, e5, e6, e7, e8],
+	 exclude=[R,G,B])
+	assert len(x) == 1
+	print(x)
+	s = x[0]
+	for name in ("Y", "U", "V"):
+		for k, v in x[0].items():
+			if k.name == name:
+				print("%s = %s" % (k, v))
+
+	"""
+	YUV2RGB
+	"""
+
+	x = sympy.solve(
+	 [e1, e2, e3, e4, e5, e6, e7, e8],
+	 exclude=[Y,U,V])
+	assert len(x) == 1
+	print(x)
+	s = x[0]
+	for name in ("R", "G", "B"):
+		for k, v in x[0].items():
+			if k.name == name:
+				print("%s = %s" % (k, v))
+
+
+
+	"""
+	BT.709 changes W_R and W_B:
+
+	- W_R = 0.2126
+	- W_B = 0.0722
+
+	"""
+
+
+
+
+	"""
+	https://en.wikipedia.org/wiki/Rec._2020
+
+	- W_R = 0.2627
+	- W_G = 0.6780
+	- W_B = 0.0593
+
+	"""
+
+		
+
 def do_lab():
 	"""
 	https://en.wikipedia.org/wiki/Lab_color_space#CIELAB-CIEXYZ_conversions
@@ -139,6 +243,7 @@ def do_lab():
 
 if __name__ == "__main__":
 	do_yuv()
+	do_ycbcr()
 	do_lab()
 
 
