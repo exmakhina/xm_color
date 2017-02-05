d73de34f7a9e575b7dfa84f740f131a5dd219d87
Jérôme Carretero <cJ@zougloub.eu>
2017-02-05 02:35:17 -0500

    conversions: fixup ycbcr->yuv naming

--- conversions.py
+++ conversions.py
@@ -18,7 +18,7 @@ import numpy as np
 Coefficients computed from conversions_computations.py
 """
 
-def rgb_to_ycbcr_bt601_fs(rgb):
+def rgb_to_yuv_bt601_fs(rgb):
 	R, G, B = rgb[...,0], rgb[...,1], rgb[...,2]
 	yuv = np.zeros_like(rgb)
 	Y = 0.114*B + 0.587*G + 0.299*R
@@ -29,7 +29,7 @@ def rgb_to_ycbcr_bt601_fs(rgb):
 	yuv[...,2] = V
 	return yuv
 
-def ycbcr_bt601_fs_to_rgb(yuv):
+def yuv_bt601_fs_to_rgb(yuv):
 	Y, U, V = yuv[...,0], yuv[...,1], yuv[...,2]
 	rgb = np.zeros_like(yuv)
 	R = 1.13983739837398*V + Y
