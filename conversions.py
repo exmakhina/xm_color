3e51b0b41533fb15c34b1bcb9b3309307914da8c
Jérôme Carretero <cJ@zougloub.eu>
2017-02-05 01:15:39 -0500

    conversions: add xyz2lab

--- conversions.py
+++ conversions.py
@@ -40,3 +40,46 @@ def ycbcr_bt601_fs_to_rgb(yuv):
 	rgb[...,2] = B
 	return rgb
 
+
+
+def xyz2lab(xyz, xn=100.0/95.047, yn=100.0/100.0, zn=100.0/108.883):
+	"""
+	Convert an image in XYZ color space to L*a*b* color space.
+
+	:param xyz: image in xyz color space, must be a floating-point image.
+	:param xn: X white point adjustment factor
+	:param yn: Y white point adjustment factor
+	:param zn: Z white point adjustment factor
+
+	References:
+
+	- https://en.wikipedia.org/wiki/Lab_color_space#CIELAB-CIEXYZ_conversions
+
+	"""
+	assert xyz.dtype in (np.float32, np.float64), xyz.dtype
+	lab = np.zeros_like(xyz)
+	x, y, z = cv2.split(xyz)
+
+	delta = 6.0/29.0
+	delta2 = (6.0**2)/(29**2)
+	delta3 = (6.0**3)/(29**3)
+
+	xb, xs = x > delta3, x <= delta3
+	yb, ys = y > delta3, y <= delta3
+	zb, zs = z > delta3, z <= delta3
+
+	fx = np.zeros_like(x)
+	fy = np.zeros_like(y)
+	fz = np.zeros_like(z)
+	fx[xb] = (x[xb]*xn)**(1./3)
+	fx[xs] = (x[xs]*xn) / (3 * delta2) + 16./116
+	fy[yb] = (y[yb]*yn)**(1./3)
+	fy[ys] = (y[ys]*yn) / (3 * delta2) + 16./116
+	fz[zb] = (z[zb]*zn)**(1./3)
+	fz[zs] = (z[zs]*zn) / (3 * delta2) + 16./116
+
+	l = 116 * fy - 16
+	a = 500 * (fx - fy)
+	b = 200 * (fy - fz)
+
+	return cv2.merge((l, a, b))
