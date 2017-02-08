f04e159d0342e656884d81604251d5ce35af0b6e
Jérôme Carretero <cJ@zougloub.eu>
2017-02-07 21:00:26 -0500

    conversions_test: add yuv back & forth

--- conversions_test.py
+++ conversions_test.py
@@ -36,6 +36,68 @@ def test_gamma():
 	img = conversions.srgb_invgamma(img)
 	imwrite("03-rgb24-invgamma.png", img)
 
-if __name__ == "__main__":
-	test_gamma()
 
+def test_ycbcr():
+	img = chart_rgb24.get_RGB24_test_img()
+
+	img = np.float64(img)/255
+	imwrite("yuv-601-00-rgb24.png", img)
+
+	img_yuv = conversions.rgb_to_ycbcr_bt601_fs(img)
+	img_rgb = conversions.ycbcr_bt601_fs_to_rgb(img_yuv)
+	imwrite("yuv-601-02-rgb.png", img_rgb)
+
+	d = np.abs(img-img_rgb)
+	_min, _max, _mean = np.min(d), np.max(d), np.mean(d)
+	print("Delta min=%f max=%f mean=%f" % (_min, _max, _mean))
+
+
+	img = chart_rgb24.get_RGB24_test_img()
+
+	img = np.float64(img)/255
+	imwrite("yuv-706-00-rgb24.png", img)
+
+	img_yuv = conversions.rgb_to_ycbcr_bt709_fs(img)
+	img_rgb = conversions.ycbcr_bt709_fs_to_rgb(img_yuv)
+	imwrite("yuv-709-02-rgb.png", img_rgb)
+
+	d = np.abs(img-img_rgb)
+	_min, _max, _mean = np.min(d), np.max(d), np.mean(d)
+	print("Delta min=%f max=%f mean=%f" % (_min, _max, _mean))
+
+
+
+if __name__ == "__main__":
+
+	import argparse
+
+	parser = argparse.ArgumentParser(
+	 description="Conversion tests",
+	)
+
+	subparsers = parser.add_subparsers(
+	 help='the command; type "%s COMMAND -h" for command-specific help' % sys.argv[0],
+	 dest='command',
+	)
+
+	for k, v in locals().items():
+		if k.startswith("test_"):
+			subp = subparsers.add_parser(
+			 k,
+			 help="",
+			)
+
+	try:
+		import argcomplete
+		argcomplete.autocomplete(parser)
+	except:
+		pass
+
+	args = parser.parse_args()
+
+	if 0:
+		pass
+
+	for k, v in locals().items():
+		if args.command == k:
+			v()
